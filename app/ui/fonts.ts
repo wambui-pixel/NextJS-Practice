cfd38825df4d55c9130ce4d7dd0f35f259fd6f97
wambuipixel <kiokowambui015@gmail.com>
2024-04-13 22:50:06 +0300

    fix: fix lusitana font
    Signed-off-by: wambuipixel <kiokowambui015@gmail.com>

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Playfair_Display } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin']});
+export const playfairDisplay = Playfair_Display({weight: ['700', '900'],subsets: ['latin']});
+export const lusitana = Lusitana({ weight: ['700'], subsets: ['latin']});
